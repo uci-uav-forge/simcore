792967e7155bbb306bc2b293f524e8c675b5c9cc
Dat-Bois <eeshvij@gmail.com>
2025-11-24 21:01:52 -0800

    Improving Alti sim

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-LOCALPATH=$(pwd)
-cd ~/ardu_ws/ || exit 1
-echo "colcon building ardu_ws..."
-colcon build > /dev/null #hides output, comment out to keep
-echo "Finished"
-cd "$LOCALPATH" || exit 1
+cd ardu_ws || exit 1
+colcon build
